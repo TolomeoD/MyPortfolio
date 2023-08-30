0a23975d170095e0f8c22005820983951e7dc1b7
unknown <tolomeod@oregonstate.edu>
2023-08-30 12:55:25 -0700

    routing set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './signature.svg';
 import './App.css';
+import Home from "./components/Home";
+import Work from "./components/Work";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Software Engineer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Work
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/work" element={<Work/>} />
+      </Routes>
+    </>
   );
 }
 
